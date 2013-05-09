32b88e5b9079fdf4963edc43508f3453c0f3faef
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-05-08 23:05:42 -0500

    auto root

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ NolaApps::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'apps#index'
 
   # See how all your routes lay out with "rake routes"
 
