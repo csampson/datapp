a5e16f33f03037ec11ae36848508e4fbfc92f9ce
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-07-23 16:27:52 -0500

    fix domain in mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,15 +1,15 @@
 class AdminMailer < ActionMailer::Base
   include SendGrid
-  default :from => "mailer@nola-apps.com"
+  default :from => "mailer@datapp.org"
 
   def app_submitted(app)
-    @domain = 'nola-apps.com'
+    @domain = 'datapp.org'
     @app = app
     mail :to => @app.contact_email, :cc => AdminUser.all.map(&:email), :subject => "App Submitted: '#{@app.name}' :-)"
   end
 
   def app_published(app)
-    @domain = 'nola-apps.com'
+    @domain = 'datapp.org'
     @app = app
     mail :to => @app.contact_email, :subject => "App Published: '#{@app.name}' :-)"
   end
